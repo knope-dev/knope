44d56e736f976039fb79e18eca4058d6e350deea
Dylan Anthony <contact@dylananthony.com>
2021-01-15 13:31:18 -0700

    feat(step): Add a new variable called IssueBranch for substituting the generating issue branch name in commands.

--- src/command.rs
+++ src/command.rs
@@ -4,6 +4,7 @@ use color_eyre::eyre::{eyre, Result, WrapErr};
 use execute::shell;
 use serde::Deserialize;
 
+use crate::git::branch_name_from_issue;
 use crate::semver::get_version;
 use crate::State;
 
@@ -15,6 +16,9 @@ pub enum Variable {
     /// ### Supported Formats
     /// 1. Cargo.toml `package.version`
     Version,
+    /// The generated branch name for the selected issue. Note that this means the workflow must
+    /// already be in [`State::IssueSelected`] when this variable is used.
+    IssueBranch,
 }
 
 /// Run the command string `command` in the current shell after replacing the keys of `variables`
@@ -25,8 +29,8 @@ pub(crate) fn run_command(
     variables: Option<HashMap<String, Variable>>,
 ) -> Result<State> {
     if let Some(variables) = variables {
-        command =
-            replace_variables(command, variables).wrap_err("While getting current version")?;
+        command = replace_variables(command, variables, &state)
+            .wrap_err("While getting current version")?;
     }
     let status = shell(command).status()?;
     if status.success() {
@@ -36,10 +40,22 @@ pub(crate) fn run_command(
 }
 
 /// Replace declared variables in the command string and return command.
-fn replace_variables(mut command: String, variables: HashMap<String, Variable>) -> Result<String> {
+fn replace_variables(
+    mut command: String,
+    variables: HashMap<String, Variable>,
+    state: &State,
+) -> Result<String> {
     for (var_name, var_type) in variables {
         match var_type {
             Variable::Version => command = command.replace(&var_name, &get_version()?.to_string()),
+            Variable::IssueBranch => {
+                match state {
+                    State::Initial(_) => return Err(eyre!("Cannot use the variable IssueBranch unless the current workflow state is IssueSelected")),
+                    State::IssueSelected(state_data) => {
+                        command = command.replace(&var_name, &branch_name_from_issue(&state_data.issue))
+                    }
+                }
+            }
         }
     }
     Ok(command)
@@ -48,6 +64,8 @@ fn replace_variables(mut command: String, variables: HashMap<String, Variable>) 
 #[cfg(test)]
 mod tests {
     use super::*;
+    use crate::issues::Issue;
+    use crate::state::{GitHub, IssueSelected};
     use tempfile::NamedTempFile;
 
     #[test]
@@ -66,15 +84,31 @@ mod tests {
 
     #[test]
     fn test_replace_variables() {
-        let command = "blah $$".to_string();
+        let command = "blah $$ branch_name".to_string();
         let mut variables = HashMap::new();
         variables.insert("$$".to_string(), Variable::Version);
+        variables.insert("branch_name".to_string(), Variable::IssueBranch);
+        let issue = Issue::GitHub {
+            number: 13,
+            title: "1234".to_string(),
+        };
+        let expected_branch_name = branch_name_from_issue(&issue);
+        let state = State::IssueSelected(IssueSelected {
+            jira_config: None,
+            github_state: GitHub::New,
+            github_config: None,
+            issue,
+        });
 
-        let command = replace_variables(command, variables).unwrap();
+        let command = replace_variables(command, variables, &state).unwrap();
 
         assert_eq!(
             command,
-            format!("blah {}", get_version().unwrap().to_string())
+            format!(
+                "blah {} {}",
+                get_version().unwrap().to_string(),
+                expected_branch_name
+            )
         )
     }
 }
