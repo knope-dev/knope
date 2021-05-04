d966d45bc3951de3b55a9104e46c6026ecea2af3
Dylan Anthony <contact@dylananthony.com>
2021-05-04 13:31:59 -0600

    feat(cli): You can now pass the name of a workflow as an argument to Dobby to bypass the selection prompt (closes #24)

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,8 @@
 #![allow(clippy::multiple_crate_versions)]
 #![forbid(unsafe_code)]
 
-use color_eyre::eyre::{Result, WrapErr};
+use clap::{crate_authors, crate_description, crate_version, App, Arg};
+use color_eyre::eyre::{ContextCompat, Result, WrapErr};
 
 use crate::config::Config;
 use crate::state::State;
@@ -27,12 +28,33 @@ mod workflow;
 fn main() -> Result<()> {
     color_eyre::install().expect("Could not set up error handling with color_eyre");
 
+    let matches = App::new("Dobby")
+        .version(crate_version!())
+        .author(crate_authors!())
+        .about(crate_description!())
+        .arg(
+            Arg::with_name("WORKFLOW")
+                .help("Name a workflow to bypass the interactive select and just run it.")
+                .index(1),
+        )
+        .get_matches();
+
+    let preselected_workflow = matches.value_of("WORKFLOW");
+
     let Config {
         workflows,
         jira,
         github,
     } = Config::load("dobby.toml").wrap_err("Could not load config file at dobby.toml")?;
-    let workflow = select(workflows, "Select a workflow")?;
+
+    let workflow = match preselected_workflow {
+        None => select(workflows, "Select a workflow")?,
+        Some(name) => workflows
+            .into_iter()
+            .find(|w| w.name == name)
+            .wrap_err_with(|| format!("No workflow named {}", name))?,
+    };
+
     let state = State::new(jira, github);
     workflow::run_workflow(workflow, state)
 }
