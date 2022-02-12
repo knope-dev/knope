794b94c4fd022de1ce028cfefeb5ea394924c0ac
Dylan Anthony <43723790+dbanty@users.noreply.github.com>
2022-02-11 19:30:29 -0700

    chore: Refactor and simplify version handling (#128)

--- src/semver.rs
+++ src/semver.rs
@@ -2,7 +2,7 @@ use std::fmt::Display;
 
 use color_eyre::eyre::WrapErr;
 use color_eyre::eyre::{eyre, Result};
-use semver::Prerelease;
+use semver::{Prerelease, Version};
 use serde::Deserialize;
 
 use crate::{package_json, pyproject};
@@ -20,69 +20,34 @@ pub(crate) enum Rule {
 }
 
 #[derive(Debug, Eq, PartialEq)]
-pub(crate) enum Version {
-    Cargo(semver::Version),
-    PyProject(semver::Version),
-    Package(semver::Version),
+pub(crate) struct PackageVersion {
+    version: Version,
+    package_manager: PackageManager,
 }
 
-impl Version {
-    fn run_on_inner<F: FnOnce(semver::Version) -> Result<semver::Version>>(
-        self,
-        func: F,
-    ) -> Result<Self> {
-        Ok(match self {
-            Version::Cargo(version) => Version::Cargo(func(version)?),
-            Version::PyProject(version) => Version::PyProject(func(version)?),
-            Version::Package(version) => Version::Package(func(version)?),
-        })
-    }
-
-    fn reset_pre(self) -> Self {
-        match self {
-            Version::Cargo(mut version) => Version::Cargo({
-                version.pre = Prerelease::EMPTY;
-                version
-            }),
-            Version::PyProject(mut version) => Version::PyProject({
-                version.pre = Prerelease::EMPTY;
-                version
-            }),
-            Version::Package(mut version) => Version::Package({
-                version.pre = Prerelease::EMPTY;
-                version
-            }),
-        }
-    }
-
-    /// Is the current version's major component 0? Useful to apply special rules in the context of
-    /// [Semantic Versioning](https://semver.org/#spec-item-4).
-    fn is_0(&self) -> bool {
-        match self {
-            Version::Cargo(v) | Version::PyProject(v) | Version::Package(v) => v.major == 0,
-        }
-    }
+#[derive(Debug, Eq, PartialEq)]
+pub(crate) enum PackageManager {
+    Cargo,
+    Poetry,
+    JavaScript,
 }
 
-impl Display for Version {
+impl Display for PackageVersion {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        match self {
-            Version::Cargo(v) | Version::PyProject(v) | Version::Package(v) => {
-                write!(f, "{}", v)
-            }
-        }
+        write!(f, "{}", self.version)
     }
 }
 
 pub(crate) fn bump_version(state: crate::State, rule: &Rule) -> Result<crate::State> {
-    if let Ok(version) = get_version() {
-        let version = bump(version, rule).wrap_err("While bumping version")?;
-        set_version(version)?;
+    if let Ok(mut package_version) = get_version() {
+        package_version.version =
+            bump(package_version.version, rule).wrap_err("While bumping version")?;
+        set_version(&package_version)?;
     }
     Ok(state)
 }
 
-pub(crate) fn get_version() -> Result<Version> {
+pub(crate) fn get_version() -> Result<PackageVersion> {
     if let Some(cargo_version) = crate::cargo::get_version("Cargo.toml") {
         let version = semver::Version::parse(&cargo_version).wrap_err_with(|| {
             format!(
@@ -90,7 +55,10 @@ pub(crate) fn get_version() -> Result<Version> {
                 cargo_version
             )
         })?;
-        Ok(Version::Cargo(version))
+        Ok(PackageVersion {
+            version,
+            package_manager: PackageManager::Cargo,
+        })
     } else if let Some(pyproject_version) = pyproject::get_version("pyproject.toml") {
         let version = semver::Version::parse(&pyproject_version).wrap_err_with(|| {
             format!(
@@ -98,7 +66,10 @@ pub(crate) fn get_version() -> Result<Version> {
                 pyproject_version
             )
         })?;
-        Ok(Version::PyProject(version))
+        Ok(PackageVersion {
+            version,
+            package_manager: PackageManager::Poetry,
+        })
     } else if let Some(package_version) = package_json::get_version("package.json") {
         let version = semver::Version::parse(&package_version).wrap_err_with(|| {
             format!(
@@ -106,57 +77,62 @@ pub(crate) fn get_version() -> Result<Version> {
                 package_version
             )
         })?;
-        Ok(Version::Package(version))
+        Ok(PackageVersion {
+            version,
+            package_manager: PackageManager::JavaScript,
+        })
     } else {
         Err(eyre!("No supported metadata found to parse version from"))
     }
 }
 
-fn set_version(version: Version) -> Result<()> {
-    match version {
-        Version::Cargo(version) => crate::cargo::set_version("Cargo.toml", &version.to_string())
+fn set_version(version: &PackageVersion) -> Result<()> {
+    match version.package_manager {
+        PackageManager::Cargo => crate::cargo::set_version("Cargo.toml", &version.to_string())
             .wrap_err("While bumping Cargo.toml"),
-        Version::PyProject(version) => {
-            pyproject::set_version("pyproject.toml", &version.to_string())
-                .wrap_err("While bumping pyproject.toml")
-        }
-        Version::Package(version) => {
+        PackageManager::Poetry => pyproject::set_version("pyproject.toml", &version.to_string())
+            .wrap_err("While bumping pyproject.toml"),
+        PackageManager::JavaScript => {
             package_json::set_version("package.json", &version.to_string())
                 .wrap_err("While bumping package.json")
         }
     }
 }
 
-/// Apply a Rule to a Version, incrementing & resetting the correct components.
+/// Apply a Rule to a [`PackageVersion`], incrementing & resetting the correct components.
 ///
 /// ### Versions 0.x
+///
 /// Versions with major component 0 have special meaning in Semantic Versioning and therefore have
 /// different behavior:
 /// 1. [`Rule::Major`] will bump the minor component.
 /// 2. [`Rule::Minor`] will bump the patch component.
-fn bump(version: Version, rule: &Rule) -> Result<Version> {
-    let is_0 = version.is_0();
+fn bump(mut version: Version, rule: &Rule) -> Result<Version> {
+    let is_0 = version.major == 0;
     match (rule, is_0) {
-        (Rule::Major, false) => version.run_on_inner(|mut v| {
-            v.major += 1;
-            v.minor = 0;
-            v.patch = 0;
-            v.pre = Prerelease::EMPTY;
-            Ok(v)
-        }),
-        (Rule::Minor, false) | (Rule::Major, true) => version.run_on_inner(|mut v| {
-            v.minor += 1;
-            v.patch = 0;
-            v.pre = Prerelease::EMPTY;
-            Ok(v)
-        }),
-        (Rule::Patch, _) | (Rule::Minor, true) => version.run_on_inner(|mut v| {
-            v.patch += 1;
-            v.pre = Prerelease::EMPTY;
-            Ok(v)
-        }),
-        (Rule::Release, _) => Ok(version.reset_pre()),
-        (Rule::Pre(prefix), _) => version.run_on_inner(|v| bump_pre(v, prefix)),
+        (Rule::Major, false) => {
+            version.major += 1;
+            version.minor = 0;
+            version.patch = 0;
+            version.pre = Prerelease::EMPTY;
+            Ok(version)
+        }
+        (Rule::Minor, false) | (Rule::Major, true) => {
+            version.minor += 1;
+            version.patch = 0;
+            version.pre = Prerelease::EMPTY;
+            Ok(version)
+        }
+        (Rule::Patch, _) | (Rule::Minor, true) => {
+            version.patch += 1;
+            version.pre = Prerelease::EMPTY;
+            Ok(version)
+        }
+        (Rule::Release, _) => {
+            version.pre = Prerelease::EMPTY;
+            Ok(version)
+        }
+        (Rule::Pre(prefix), _) => bump_pre(version, prefix),
     }
 }
 
@@ -166,73 +142,70 @@ mod test_bump {
 
     #[test]
     fn major() {
-        let version = Version::Cargo(semver::Version::new(1, 2, 3));
+        let version = Version::new(1, 2, 3);
         let version = bump(version, &Rule::Major).unwrap();
 
-        assert_eq!(version, Version::Cargo(semver::Version::new(2, 0, 0)));
+        assert_eq!(version, Version::new(2, 0, 0));
     }
 
     #[test]
     fn major_0() {
-        let version = Version::Cargo(semver::Version::new(0, 1, 2));
+        let version = Version::new(0, 1, 2);
         let version = bump(version, &Rule::Major).unwrap();
 
-        assert_eq!(version, Version::Cargo(semver::Version::new(0, 2, 0)));
+        assert_eq!(version, Version::new(0, 2, 0));
     }
 
     #[test]
     fn minor() {
-        let version = Version::Cargo(semver::Version::new(1, 2, 3));
+        let version = Version::new(1, 2, 3);
         let version = bump(version, &Rule::Minor).unwrap();
 
-        assert_eq!(version, Version::Cargo(semver::Version::new(1, 3, 0)));
+        assert_eq!(version, Version::new(1, 3, 0));
     }
 
     #[test]
     fn minor_0() {
-        let version = Version::Cargo(semver::Version::new(0, 1, 2));
+        let version = Version::new(0, 1, 2);
         let version = bump(version, &Rule::Minor).unwrap();
 
-        assert_eq!(version, Version::Cargo(semver::Version::new(0, 1, 3)));
+        assert_eq!(version, Version::new(0, 1, 3));
     }
 
     #[test]
     fn patch() {
-        let version = Version::Cargo(semver::Version::new(1, 2, 3));
+        let version = Version::new(1, 2, 3);
         let version = bump(version, &Rule::Patch).unwrap();
 
-        assert_eq!(version, Version::Cargo(semver::Version::new(1, 2, 4)));
+        assert_eq!(version, Version::new(1, 2, 4));
     }
 
     #[test]
     fn patch_0() {
-        let version = Version::Cargo(semver::Version::new(1, 2, 3));
+        let version = Version::new(1, 2, 3);
         let version = bump(version, &Rule::Patch).unwrap();
 
-        assert_eq!(version, Version::Cargo(semver::Version::new(1, 2, 4)));
+        assert_eq!(version, Version::new(1, 2, 4));
     }
 
     #[test]
     fn pre() {
-        let version = Version::Cargo(semver::Version::new(1, 2, 3));
+        let version = Version::new(1, 2, 3);
         let version = bump(version, &Rule::Pre("rc".to_string())).unwrap();
 
-        assert_eq!(
-            version,
-            Version::Cargo(semver::Version::parse("1.2.3-rc.0").unwrap())
-        );
+        assert_eq!(version, Version::parse("1.2.3-rc.0").unwrap());
     }
 
     #[test]
     fn release() {
-        let version = Version::Cargo(semver::Version::parse("1.2.3-rc.0").unwrap());
+        let version = Version::parse("1.2.3-rc.0").unwrap();
         let version = bump(version, &Rule::Release).unwrap();
 
-        assert_eq!(version, Version::Cargo(semver::Version::new(1, 2, 3)));
+        assert_eq!(version, Version::new(1, 2, 3));
     }
 }
 
-fn bump_pre(mut version: semver::Version, prefix: &str) -> Result<semver::Version> {
+fn bump_pre(mut version: Version, prefix: &str) -> Result<Version> {
     if version.pre.is_empty() {
         version.pre = Prerelease::new(&format!("{}.0", prefix))?;
         return Ok(version);
