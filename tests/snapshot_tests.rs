d16fe69aa32c08eea61cc17664c639ab43931535
Dylan Anthony <contact@dylananthony.com>
2022-06-05 21:31:05 -0600

    test: Add a test for attempting to bump multiple files at once in a package.

--- tests/snapshot_tests.rs
+++ tests/snapshot_tests.rs
@@ -367,6 +367,62 @@ fn test_prepare_release_multiple_packages() {
     }
 }
 
+/// Run a `PrepareRelease` in a repo with multiple files in a package set to verify error message.
+#[test]
+fn test_prepare_release_multiple_files_in_package() {
+    // Arrange.
+    let temp_dir = tempfile::tempdir().unwrap();
+    let temp_path = temp_dir.path();
+    let source_path = Path::new("tests/prepare_release_package_selection");
+
+    init(temp_path);
+    commit(temp_path, "feat: Existing feature");
+    tag(temp_path, "1.0.0");
+    commit(temp_path, "feat: New feature");
+
+    let knope_toml = "multiple_files_in_package_knope.toml";
+    copy(source_path.join(&knope_toml), temp_path.join("knope.toml")).unwrap();
+    for file in [
+        "CHANGELOG.md",
+        "Cargo.toml",
+        "pyproject.toml",
+        "package.json",
+    ] {
+        copy(source_path.join(file), temp_path.join(file)).unwrap();
+    }
+
+    // Act.
+    let dry_run_assert = Command::new(cargo_bin!("knope"))
+        .arg("release")
+        .arg("--dry-run")
+        .current_dir(temp_dir.path())
+        .assert();
+    let actual_assert = Command::new(cargo_bin!("knope"))
+        .arg("release")
+        .current_dir(temp_dir.path())
+        .assert();
+
+    // Assert.
+    dry_run_assert
+        .failure()
+        .stderr_eq_path(source_path.join("multiple_files_in_package_dry_run_output.txt"));
+    actual_assert
+        .failure()
+        .stderr_eq_path(source_path.join("multiple_files_in_package_output.txt"));
+
+    // Nothing should change because it errored.
+    assert_eq_path(
+        source_path.join("CHANGELOG.md"),
+        read_to_string(temp_path.join("CHANGELOG.md")).unwrap(),
+    );
+    for file in ["Cargo.toml", "pyproject.toml", "package.json"] {
+        assert_eq_path(
+            source_path.join(file),
+            read_to_string(temp_path.join(file)).unwrap(),
+        );
+    }
+}
+
 /// Run a `PrepareRelease` in a repo and verify that the changelog is updated based on config.
 #[rstest]
 #[case(Some("CHANGELOG.md"))]
