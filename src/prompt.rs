214811524a56362c0bc9adc5d84bb5f79be40cab
Dylan Anthony <dbanty@users.noreply.github.com>
2023-06-06 21:45:17 -0600

    chore: Remove dialoguer, ignore rstest, finish syn 2 migration

--- src/prompt.rs
+++ src/prompt.rs
@@ -1,28 +1,16 @@
+use inquire::{Select, Text};
 use std::fmt::Display;
 
-use console::Term;
-use dialoguer::{theme::ColorfulTheme, Input, Select};
 use miette::Result;
 
 use crate::step::StepError;
 
-pub(crate) fn select<T: Display>(mut items: Vec<T>, prompt: &str) -> Result<T, StepError> {
-    let selection = Select::with_theme(&ColorfulTheme::default())
-        .items(&items)
-        .default(0)
-        .with_prompt(prompt)
-        .interact_on_opt(&Term::stdout())
-        .map_err(|e| StepError::UserInput(Some(e)))?;
-
-    match selection {
-        Some(index) => Ok(items.remove(index)),
-        None => Err(StepError::UserInput(None)),
-    }
+pub(crate) fn select<T: Display>(items: Vec<T>, prompt: &str) -> Result<T, StepError> {
+    Select::new(prompt, items)
+        .prompt()
+        .map_err(StepError::UserInput)
 }
 
 pub(crate) fn get_input(prompt: &str) -> Result<String, StepError> {
-    Input::with_theme(&ColorfulTheme::default())
-        .with_prompt(prompt)
-        .interact_text()
-        .map_err(|e| StepError::UserInput(Some(e)))
+    Text::new(prompt).prompt().map_err(StepError::UserInput)
 }
