e68a3d527e134bd4b45f624b3eca47a7c9f25c77
Dylan Anthony <dbanty@users.noreply.github.com>
2023-11-01 19:51:48 -0600

    docs: Start Starlight

---
+++ starlight/astro.config.mjs
@@ -0,0 +1,42 @@
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
+
+import expressiveCode from "astro-expressive-code";
+
+/** @type {import('astro-expressive-code').AstroExpressiveCodeOptions} */
+const expressiveCodeOptions = {
+  // Example: Change the theme to "dracula"
+  theme: ["github-light", "github-dark"],
+  frames: {
+    removeCommentsWhenCopyingTerminalFrames: true,
+  },
+};
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [
+    starlight({
+      title: "Knope",
+      social: {
+        github: "https://github.com/knope-dev/knope",
+      },
+      customCss: ["./src/custom.css"],
+      sidebar: [
+        { label: "Installation", link: "/installation" },
+        {
+          label: "Tutorials",
+          autogenerate: {
+            directory: "tutorials",
+          },
+        },
+        {
+          label: "Reference",
+          autogenerate: {
+            directory: "reference",
+          },
+        },
+      ],
+    }),
+    expressiveCode(expressiveCodeOptions),
+  ],
+});
