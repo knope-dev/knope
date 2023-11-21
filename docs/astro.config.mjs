5a8c8b03303214afdd2d4aedae1a3708cf1064a1
Dylan Anthony <43723790+dbanty@users.noreply.github.com>
2023-11-20 21:39:30 -0700

    Update astro.config.mjs

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from "astro/config";
 import starlight from "@astrojs/starlight";
 
-import starlightLinksValidator from "starlight-links-validator";e
+import starlightLinksValidator from "starlight-links-validator";
 
 // https://astro.build/config
 export default defineConfig({
