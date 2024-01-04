21579543d2d5c6b08f11682dd5df902629222230
FallenValkyrie <e.vanemelen@proton.me>
2024-01-04 01:11:37 +0100

    Add Gitea integration to CreatePullRequest step

--- src/step/create_pull_request.rs
+++ src/step/create_pull_request.rs
@@ -1,7 +1,7 @@
 use miette::Diagnostic;
 
 use crate::{
-    integrations::github::{create_or_update_pull_request, CreatePullRequestError},
+    integrations::{gitea, github},
     state::RunType,
     variables,
     variables::{replace_variables, Template},
@@ -17,18 +17,34 @@ pub(super) fn run(
     let (mut state, mut dry_run) = run_type.decompose();
     let title = replace_variables(title, &state)?;
     let body = replace_variables(body, &state)?;
-    let Some(github_config) = &state.github_config else {
+
+    if state.github_config.is_none() && state.gitea_config.is_none() {
         return Err(Error::NotConfigured);
-    };
-    state.github = create_or_update_pull_request(
-        &title,
-        &body,
-        base,
-        state.github,
-        github_config,
-        &mut dry_run,
-        Verbose::Yes,
-    )?;
+    }
+
+    if let Some(github_config) = &state.github_config {
+        state.github = github::create_or_update_pull_request(
+            &title,
+            &body,
+            base,
+            state.github,
+            github_config,
+            &mut dry_run,
+            Verbose::Yes,
+        )?;
+    }
+
+    if let Some(gitea_config) = &state.gitea_config {
+        state.gitea = gitea::create_or_update_pull_request(
+            &title,
+            &body,
+            base,
+            state.gitea,
+            gitea_config,
+            &mut dry_run,
+            Verbose::Yes,
+        )?;
+    }
     Ok(RunType::recompose(state, dry_run))
 }
 
@@ -37,14 +53,17 @@ pub(crate) enum Error {
     #[error(transparent)]
     #[diagnostic(transparent)]
     Variables(#[from] variables::Error),
-    #[error("GitHub is not configured")]
+    #[error("No forge has been configured")]
     #[diagnostic(
-        code(create_pull_request::github::not_configured),
-        help("GitHub must be configured in order to use the CreatePullRequest step"),
-        url("https://knope.tech/reference/config-file/github/")
+        code(create_pull_request::forge::not_configured),
+        help("A forge must be configured in order to use the CreatePullRequest step"),
+        url("https://knope.tech/reference/concepts/forge/")
     )]
     NotConfigured,
     #[error(transparent)]
     #[diagnostic(transparent)]
-    GitHub(#[from] CreatePullRequestError),
+    GitHub(#[from] github::CreatePullRequestError),
+    #[error(transparent)]
+    #[diagnostic(transparent)]
+    Gitea(#[from] gitea::CreatePullRequestError),
 }
